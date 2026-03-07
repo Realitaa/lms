59000f9809b22799e0c3c3333870f1b88068af66
Reza Mulia Putra <74757588+Realitaa@users.noreply.github.com>
2026-03-07 15:21:45 +0700

    feat: create module and lesson control

--- resources/js/pages/modules/Index.vue
+++ resources/js/pages/modules/Index.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
 import { Head, Link, router } from '@inertiajs/vue3';
-import { ArrowLeft } from 'lucide-vue-next';
+import { Plus, Trash2 } from 'lucide-vue-next';
 import { ref } from 'vue';
 import { toast } from 'vue-sonner';
 import CourseForm from '@/components/courses/CourseForm.vue';
-import { Button } from '@/components/ui/button';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { update } from '@/routes/courses';
 import type { BreadcrumbItem, Course } from '@/types';
 import TitleWithBack from '@/components/TitleWithBack.vue';
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 const props = defineProps<{
   course: Course;
@@ -46,6 +58,10 @@ function onSubmit(formData: FormData) {
     },
   });
 }
+
+const tags = Array.from({ length: 50 }).map(
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`,
+)
 </script>
 
 <template>
@@ -60,6 +76,51 @@ function onSubmit(formData: FormData) {
         :subtitle="`Kelola modul pada kursus ${course.title}`"
       />
 
+    </div>
+    <div class="w-full flex gap-4 mx-4 h-[calc(100vh-200px)]">
+      <div class="w-[67%] border">
+        editor
+      </div>
+      <Card class="w-2/7 h-full flex flex-col p-4">
+        <CardHeader class="px-0">
+          <CardTitle>Modul dan Materi</CardTitle>
+          <CardDescription>
+            Drag & Drop untuk reposisi materi atau modul. Tombol + disamping untuk menambah modul.
+          </CardDescription>
+          <CardAction>
+            <Button size="icon" variant="ghost">
+              <Plus />
+            </Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent class="flex-1 overflow-hidden px-0">
+          <ScrollArea class="h-full pr-3">
+              <template v-for="tag in tags" :key="tag">
+                <Card class="w-full max-w-sm mb-2 p-4 gap-2!">
+                  <CardHeader class="px-0 pb-2! border-b">
+                    <CardTitle>Pengenalan Komputer dan Penggunaan Singkat</CardTitle>
+                    <CardDescription>
+                      Modul / Tes
+                    </CardDescription>
+                    <CardAction>
+                      <Button size="icon" variant="ghost" class="text-destructive hover:text-destructive">
+                        <Trash2 />
+                      </Button>
+                    </CardAction>
+                  </CardHeader>
+                  <CardContent class="px-0">
+                    <div v-for="i in 10" :key="i" class="flex justify-between border-b py-2">
+                      <p class="font-bold text-sm">Apa itu BIOS (Basic Input Output System)?</p>
+                      <Button size="icon" variant="ghost" class="text-destructive hover:text-destructive">
+                        <Trash2 />
+                      </Button>
+                    </div>
+                  </CardContent>
+                </Card>
+              </template>
+          </ScrollArea>
+        </CardContent>
+      </Card>
     </div>
   </AppLayout>
 </template>
